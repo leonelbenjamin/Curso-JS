83d20a2f4b3886683c2bd4ff8eaae15a634f5b5d
Leonel Mercau <leonelbenjaminmercau@gmail.com>
2022-06-29 03:45:01 -0300

    Acceder a valores de un Array

---
+++ Curso JS Moderno/08-Arrays/js/02-app.js
@@ -0,0 +1,14 @@
+//Acceder a los Arreglos 
+
+const numeros = [10,20,30,40,50,[1,2,3]];
+
+console.log(numeros);
+console.table(numeros);
+
+//acceder al arreglo 
+
+console.log(numeros[2]);
+console.log(numeros[0]);
+
+//Acceder a el arreglo de un arreglo
+console.log(numeros[5][0]);
