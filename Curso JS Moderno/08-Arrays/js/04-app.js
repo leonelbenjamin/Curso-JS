bcc623b4c551306f18bbf6a8776c99f6ce0b3f67
Leonel Mercau <leonelbenjaminmercau@gmail.com>
2022-06-29 16:51:44 -0300

    04 array- Añadir elementos a un Array

---
+++ Curso JS Moderno/08-Arrays/js/04-app.js
@@ -0,0 +1,6 @@
+const meses = ['Enero','Febrero', 'Marzo', 'Abril','Mayo', 'Junio'];
+
+meses[0] = 'Nuevo Mes';     //Editar un valor 
+meses[6] = 'Ultimo Mes';    //Añadir un valor nuevo 
+
+console.table(meses);
