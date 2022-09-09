784fe5aa69629e3ac992cba7a2aeeaf9abf39c48
Leonel Mercau <leonelbenjaminmercau@gmail.com>
2022-09-08 21:20:27 -0300

    arrays clase 9

---
+++ Curso JS Moderno/08-Arrays/js/09-app.js
@@ -0,0 +1,22 @@
+const carrito = [
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'tv', precio: 600},
+    {nombre: 'PC', precio: 700},
+    {nombre: 'tablet', precio: 800}
+]
+//Accede a el array de todos los objetos
+for (let i = 0; i < carrito.length; i++) {
+    console.log( carrito[i]);
+}
+
+//Accede a las propiedades de los objetos con la contatenacion y el nombre de la propiedad
+for (let i = 0; i < carrito.length; i++) {
+    console.log( carrito[i].nombre);
+}
+for (let i = 0; i < carrito.length; i++) {
+    console.log( carrito[i].precio);
+}
+//
+for (let i = 0; i < carrito.length; i++) {
+    console.log(`${carrito[i].nombre} - Precio: ${carrito[i].precio}`);
+}
